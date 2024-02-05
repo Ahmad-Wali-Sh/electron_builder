48a690e4ebd9a6f36a224b652134936d2c2fabab
Ahmad Wali <ahmadwalisharifybtc@gmail.com>
2024-02-05 14:07:26 +0430

    backup progress evolved.

--- main.js
+++ main.js
@@ -12,6 +12,54 @@ const fs = require("fs");
 let mainWindow;
 let backendProcess;
 
+
+function getPathOfUSB(deviceName) {
+  return new Promise((resolve, reject) => {
+    const command = `powershell.exe -Command "Get-WmiObject Win32_Volume | Where-Object { $_.Label -eq '${deviceName}' } | Select-Object -ExpandProperty Name"`;
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error("Error:", error);
+        reject(error);
+        return;
+      }
+      if (stderr) {
+        console.error("PowerShell Error:", stderr);
+        reject(stderr);
+        return;
+      }
+      const path = stdout.trim();
+      if (path) {
+        resolve(path);
+      } else {
+        reject(`USB device with name '${deviceName}' not found.`);
+        console.log("USB device not found.");
+      }
+    });
+  });
+}
+
+const backup = () => {
+  const usbName = "backup_dokht";
+  const dbPath = path.join(
+    process.resourcesPath,
+    "run_server/_internal/db.sqlite3"
+  );
+  getPathOfUSB(usbName)
+    .then((usbPath) => {
+      console.log("USB device path:", usbPath);
+      fs.copyFile(dbPath, usbPath + '/db.sqlite3', (err) => {
+        if (err) {
+          console.error("Error copying file:", err);
+        } else {
+          console.log("Backup successful.");
+        }
+      });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
 const loadURL = serve({ directory: path.join(__dirname, "frontend") });
 app.on("ready", () => {
   // Check if the allowed USB device is connected
@@ -51,53 +99,6 @@ app.on("ready", () => {
           });
         });
 
-        function getPathOfUSB(deviceName) {
-          return new Promise((resolve, reject) => {
-            const command = `powershell.exe -Command "Get-WmiObject Win32_Volume | Where-Object { $_.Label -eq '${deviceName}' } | Select-Object -ExpandProperty Name"`;
-            exec(command, (error, stdout, stderr) => {
-              if (error) {
-                console.error("Error:", error);
-                reject(error);
-                return;
-              }
-              if (stderr) {
-                console.error("PowerShell Error:", stderr);
-                reject(stderr);
-                return;
-              }
-              const path = stdout.trim();
-              if (path) {
-                resolve(path);
-              } else {
-                reject(`USB device with name '${deviceName}' not found.`);
-                console.log("USB device not found.");
-              }
-            });
-          });
-        }
-
-        const backup = () => {
-          const usbName = "backup_dokht";
-          const dbPath = path.join(
-            process.resourcesPath,
-            "run_server/_internal/db.sqlite3"
-          );
-          getPathOfUSB(usbName)
-            .then((usbPath) => {
-              console.log("USB device path:", usbPath);
-              fs.copyFile(dbPath, usbPath, (err) => {
-                if (err) {
-                  console.error("Error copying file:", err);
-                } else {
-                  console.log("Backup successful.");
-                }
-              });
-            })
-            .catch((error) => {
-              console.error("Error:", error);
-            });
-        };
-
         mainWindow.on("closed", () => {
           mainWindow = null;
           backendProcess.kill();
@@ -113,3 +114,8 @@ app.on("ready", () => {
       console.error("Error executing PowerShell command:", error);
     });
 });
+
+
+app.on('window-all-closed', () => {
+  backup()
+})
